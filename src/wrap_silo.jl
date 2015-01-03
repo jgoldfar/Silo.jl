52683822ceb794322b0aa2d4216f5f2405626bc6
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-01-03 11:25:25 -0500

    Updated wrap file to more closely match Sundials.jl's (hoping it works out better?)

--- src/wrap_silo.jl
+++ src/wrap_silo.jl
@@ -2,18 +2,29 @@
 # This file is not an active part of the package. This is the code
 # that uses the Clang.jl package to wrap Silo using the headers.
 
-import Clang.wrap_c
+cd(dirname(@__FILE__))
 
-silo_includes = map(
-  x->joinpath("/Users/jgoldfar/Documents/work/projects/deps/usr/include", x),
-  [
-    "ioperf.h",
-    "pmpio.h",
-    "silo.h",
-    ])
+using Clang.wrap_c
 
-# check_use_header(path) = true
-context = Clang.wrap_c.init()
-wrap_c.wrap_c_headers(context, silo_includes)
+silo_names = ["silo.h",]
 
+#Clang include path for system clang: /usr/include/clang/3.4/include
+
+clang_includes = [
+  "/usr/include/clang/3.4/include",
+  "/usr/include",
+  "/usr/include/llvm-3.4/llvm",
+  "/usr/include/llvm-c-3.4/llvm-c",
+  ]
+
+check_use_header(path) = true
+
+clang_extraargs = ["-D", "__STDC_LIMIT_MACROS", "-D", "__STDC_CONSTANT_MACROS"]
+context = wrap_c.init(output_file = "silo.jl", clang_args = clang_extraargs, clang_includes = clang_includes, common_file = "libsilo_common.jl")
+context.options.wrap_structs = true
+
+    path = "/home/jgoldfar/Documents/work/projects/deps/usr/include"
+    headers = map(x->joinpath(path, x),split(readall(`ls $path`)) )
+    @show headers
+    wrap_c.wrap_c_headers(context, headers)
 mv("silo.jl", "libsilo.jl")  # avoid a name conflict for case-insensitive file systems
