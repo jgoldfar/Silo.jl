f902874939228cce24c1211426ee21b40498721c
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-02 20:43:18 -0500

    Cleaned inclusion of deps/deps.jl

--- src/Silo.jl
+++ src/Silo.jl
@@ -1,7 +1,7 @@
 # Silo.jl: Wrapper around the Silo library (https://wci.llnl.gov/simulation/computer-codes/silo)
 module Silo
 if isfile(joinpath(dirname(dirname(@__FILE__)),"deps","deps.jl"))
-    include("../deps/deps.jl")
+    include(joinpath(dirname(dirname(@__FILE__)),"deps","deps.jl"))
 else
     error("Silo not properly installed. Please run Pkg.build(\"Silo\")")
 end
