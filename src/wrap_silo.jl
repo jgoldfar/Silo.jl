873e481e83ed10c1c11e503c1fa28b4269463230
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-02 21:43:23 -0500

    Simplified and improved (hopefully) wrap_silo file for structure of silo include files.

--- src/wrap_silo.jl
+++ src/wrap_silo.jl
@@ -2,27 +2,18 @@
 # This file is not an active part of the package. This is the code
 # that uses the Clang.jl package to wrap Silo using the headers.
 
-using Clang.wrap_c
+import Clang.wrap_c
 
-if (!has(ENV, "JULIAHOME"))
-  error("Please set JULIAHOME variable to the root of your julia install")
-end
+silo_includes = map(
+  x->joinpath("/Users/jgoldfar/Documents/work/projects/deps/usr/include", x),
+  [
+    "ioperf.h",
+    "pmpio.h",
+    "silo.h",
+    ])
 
-clang_includes = map(x->joinpath(ENV["JULIAHOME"], x), [
-  "deps/llvm-3.2/build/Release/lib/clang/3.2/include",
-  "deps/llvm-3.2/include",
-  "deps/llvm-3.2/include",
-  "deps/llvm-3.2/build/include/",
-  "deps/llvm-3.2/include/"
-  ])
-silo_includes = map(x->joinpath(ENV["JULIAHOME"], x), [
-  "deps/silo-...-/usr/",
-  ])
-silo_names = ["silo",]
-check_use_header(path) = true
-for name in silo_includes
-    headers = map(x->joinpath(path, x), split(readall(`ls $path` | `sort`)) )
-    @show headers
-    wrap_c.wrap_c_headers(headers, [clang_includes, path], ASCIIString[], check_use_header, "$name.jl")
-end
+# check_use_header(path) = true
+context = Clang.wrap_c.init()
+wrap_c.wrap_c_headers(context, silo_includes)
+
 mv("silo.jl", "libsilo.jl")  # avoid a name conflict for case-insensitive file systems
