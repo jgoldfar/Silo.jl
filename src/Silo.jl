1e7c8e2aa564542705c1af7066377c18a292e48c
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-10 17:47:01 -0500

    Added sample of write(…) for vectors, based on WENO scheme code

--- src/Silo.jl
+++ src/Silo.jl
@@ -6,4 +6,25 @@ else
     error("Silo not properly installed. Please run Pkg.build(\"Silo\")")
 end
 
+
+# DBfile       *dbfile = NULL;
+#     DBoptlist	*dbopt = DBMakeOptlist(1);
+#     char         fn[100], title[100];
+#     int     lbuf = ndp->rgr.lbuf;
+#     int     ubuf = ndp->rgr.ubuf;
+#     int     gmax = ndp->rgr.gmax;
+#     int     N = gmax-lbuf-ubuf;
+
+#     sprintf(fn,"./output/u%s_step%d.silo",ndp->par.output_fn,ndp->step);
+#     dbfile = DBCreate(fn, DB_CLOBBER, DB_LOCAL, "Line Output", DB_PDB);
+
+#     sprintf(title,"time: %lf",ndp->time);
+#     DBAddOption(dbopt, DBOPT_LABEL, &title);
+#     DBPutCurve(dbfile,"Numerical_Solution",x,y1,DB_FLOAT,N+1,NULL);
+#     DBPutCurve(dbfile,"Analytical_Solution",x,y2,DB_FLOAT,N+1,NULL);
+#     DBPutCurve(dbfile,"Error",x,y3,DB_FLOAT,N+1,NULL);
+#     DBClose(dbfile);
+
+# 	DBFreeOptlist(dbopt);
+
 end # module
