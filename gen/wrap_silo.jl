1b644024d8e42636b3d177b50067eb9a5b448d48
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-01-19 11:50:25 -0500

    Simplified wrap_silo.jl script per eglib.jl (https://github.com/ihnorton/Eglib.jl) example. Added output from script run, and opening a Clang.jl issue

--- gen/wrap_silo.jl
+++ gen/wrap_silo.jl
@@ -6,25 +6,33 @@ cd(dirname(@__FILE__))
 
 using Clang.wrap_c
 
-silo_names = ["silo.h",]
+# silo_names = ["silo.h",]
 
 #Clang include path for system clang: /usr/include/clang/3.4/include
 
-clang_includes = [
-  "/usr/include/clang/3.4/include",
-  "/usr/include",
-  "/usr/include/llvm-3.4/llvm",
-  "/usr/include/llvm-c-3.4/llvm-c",
-  ]
+JULIAHOME = "/home/jgoldfar/Documents/work/projects/julia"
 
+clang_includes = map(x->joinpath(JULIAHOME, x),[
+                       "usr/include/clang/",
+                       "usr/include",
+                       "usr/include/llvm",
+                       "usr/include/llvm-c",
+                       ]
+                     )
+
+path = "/home/jgoldfar/Documents/work/projects/Silo.jl/deps/usr/include"
 check_use_header(path) = true
 
-clang_extraargs = ["-D", "__STDC_LIMIT_MACROS", "-D", "__STDC_CONSTANT_MACROS"]
-context = wrap_c.init(output_file = "silo.jl", clang_args = clang_extraargs, clang_includes = clang_includes, common_file = "libsilo_common.jl")
+clang_extraargs = ["-D", "__STDC_LIMIT_MACROS", "-D", "__STDC_CONSTANT_MACROS", "-v", "-analyze"]
+context = wrap_c.init(output_file = "silo.jl",
+                      header_library=x->"silo",
+                      clang_args = clang_extraargs,
+                      clang_includes = clang_includes,
+                      common_file = "libsilo_h.jl"
+                      )
 context.options.wrap_structs = true
 
-    path = "/home/jgoldfar/Documents/work/projects/deps/usr/include"
-    headers = map(x->joinpath(path, x),split(readall(`ls $path`)) )
-    @show headers
-    wrap_c.wrap_c_headers(context, headers)
+# headers = map(x->joinpath(path, x), split(readall(`ls $path`)) )
+# @show headers
+wrap_c.wrap_c_headers(context, [joinpath(path, "silo.h")])
 mv("silo.jl", "libsilo.jl")  # avoid a name conflict for case-insensitive file systems
