c0b35cb60e281d462a2d370aeaa2809d16ec9abe
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-20 11:51:28 -0500

    Continuing work on post-processing shell scripts to generate constants

--- deps/const_pre.sh
+++ deps/const_pre.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 constfile1=gen/const_pre.jl
-constfile2=gen/const_hand.jl
 
 grep "#define\s*\x*" ../deps/usr/include/silo.h > "${constfile1}"
-grep "\\\n*" ../deps/usr/include/silo.h > "${constfile2}"
 
-echo "Post-process the include/constant file ${constfile1} by hand now. The macros/multi-line defines are in ${constfile2}."
+echo "Post-process the include/constant file ${constfile1} by hand now."
--- deps/const_pre_2.sh
+++ deps/const_pre_2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -e "s/#define\s*\([A-Za-z0-9_]*\)\s*\([A-Za-z0-9_]*\)/const\t\1 = \2/ " -e "s/\/\*/#=/" -e "s/\*\//=#/" < gen/const_pre.jl > gen/const_pre_2.jl
+sed -e "s/#define[ ]*\(\w*\)[ ]*\(\w*\)/const \1 = \2/g" -e "s/\/\*/#=/" -e "s/\*\//=#/" < gen/const_pre.jl > gen/const_pre_2.jl
