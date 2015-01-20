634f7ea692ac9bf35b1ee0ec8712bd9aed882e3e
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-20 17:47:20 -0500

    Added a test for writing 1d arrays to files (working.)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,5 @@
 # Run package tests
 include(joinpath("..", "src", "Silo.jl"))
 # run(`cd $(dirname(@__FILE__))/files && make`)
-# Silo.Version()
-# v2 = Silo._VERSION
-println("Silo version ", Silo._VERSION)
-println(Silo.VersionGE(3, 0, 0)) #=true)
+
+include("test1dwrite.jl")
