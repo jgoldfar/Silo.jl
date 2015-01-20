e3b77d708ef759e622a8d21e74fb5370eedf0aed
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-20 12:32:39 -0500

    We process constants in a semi-automated way now.

--- deps/const_pre_1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-constfile2=gen/const_hand.jl
-
-grep -n "\\\n*" ../deps/usr/include/silo.h > "${constfile2}"
-
-echo "The macros/multi-line defines are in ${constfile2}."
--- deps/const_pre_2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sed -e "s/#define[ ]*\(\w*\)[ ]*\(\w*\)/const \1 = \2/g" -e "s/\/\*/#=/" -e "s/\*\//=#/" < gen/const_pre.jl > gen/const_pre_2.jl
