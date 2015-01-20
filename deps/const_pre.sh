d65f753cd3251fac29703340ebcde5e0614ac01a
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-20 12:32:39 -0500

    We process constants in a semi-automated way now.

--- deps/const_pre.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-constfile1=gen/const_pre.jl
-
-grep "#define\s*\x*" ../deps/usr/include/silo.h > "${constfile1}"
-
-echo "Post-process the include/constant file ${constfile1} by hand now."
