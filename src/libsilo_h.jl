eaee56ef5b649032e319523c78f809d37b1fe501
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-21 18:09:20 -0500

    Added a map between Silo’s Datatype enumeration and what we need to do block reinterpretation in Julia.

--- src/libsilo_h.jl
+++ src/libsilo_h.jl
@@ -238,10 +238,11 @@ const DB_CHAR = 21
 const DB_LONG_LONG = 22
 const DB_NOTYPE = 25           #=unknown type =#
 
-const JuliaSilotypemap=[:Int64 => DB_LONG_LONG, ]
-const SiloJuliatypemap=[DB_LONG_LONG => :Int64, ] # Previous inverse
-const enummap=[:DB_INT => DB_INT,]
-const enummapinv=[DB_INT => :DB_INT] # Previous inverse
+const JuliaSilotypemap=[Int64 => DB_LONG_LONG, Int32 => DB_INT, Int16 =>DB_SHORT, Int8=>DB_CHAR, Float32 => DB_FLOAT, Float64 => DB_DOUBLE]
+# const CJuliaSilotypemap=[Clonglong => DB_LONG_LONG, Cint => DB_INT, Cshort =>DB_SHORT, Cchar=>DB_CHAR, Cfloat => DB_FLOAT, Cdouble => DB_DOUBLE]
+const SiloJuliatypemap=[DB_LONG_LONG => Int64, DB_INT=> Int32, DB_SHORT=>Int16, DB_CHAR=>Int8, DB_FLOAT=>Float32, DB_DOUBLE=>Float64 ]
+# const SiloCJuliatypemap=[DB_LONG_LONG => Clonglong, DB_INT=> Cint, DB_SHORT=>Cshort , DB_CHAR=>Cchar, Cfloat => DB_FLOAT, DB_DOUBLE=>Cdouble]
+
 end #module
 
 #= Flags for DBCreate =#
