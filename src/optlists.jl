c8c13a9221260dbff8c0e984c349437421c8271e
Jonathan Goldfarb <jgoldfar@new-mac>
2015-01-21 18:16:52 -0500

    Added TODOs for optlists.jl interface

--- src/optlists.jl
+++ src/optlists.jl
@@ -1,1 +1,6 @@
 # options.jl: Wrapper for internal DBoptlist objects, with Julia support
+
+#TODO: define
+# - convert(Ptr{DBoptlist}, silooptions)
+# - convert(silooptions, Ptr{DBoptlist})
+# - define push!/pop!/setindex!/getindex for silooptions
